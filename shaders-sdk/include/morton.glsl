ace2a7b56adb3f5aa1d628e32d32b6004c747f27
Alexa Save <33594593+elviras9t@users.noreply.github.com>
2018-02-08 06:15:09 +0800

    optimize morton codes

--- shaders-sdk/include/morton.glsl
+++ shaders-sdk/include/morton.glsl
@@ -3,19 +3,17 @@
 
 // method to seperate bits from a given integer 3 positions apart
 uint64_t splitBy3(in uint a){
-    uint64_t x = a & 0x1ffffful; // we only look at the first 21 bits
-    x = (x | x << 32) & 0x1f00000000fffful;  // shift left 32 bits, OR with self, and 00011111000000000000000000000000000000001111111111111111
-    x = (x | x << 16) & 0x1f0000ff0000fful;  // shift left 32 bits, OR with self, and 00011111000000000000000011111111000000000000000011111111
-    x = (x | x << 8) & 0x100f00f00f00f00ful; // shift left 32 bits, OR with self, and 0001000000001111000000001111000000001111000000001111000000000000
-    x = (x | x << 4) & 0x10c30c30c30c30c3ul; // shift left 32 bits, OR with self, and 0001000011000011000011000011000011000011000011000011000100000000
-    x = (x | x << 2) & 0x1249249249249249ul;
+    uint64_t x = uint64_t(a) & 0x1ffffful; // we only look at the first 21 bits
+    x = (x | x << 32ul) & 0x1f00000000fffful;  // shift left 32 bits, OR with self, and 00011111000000000000000000000000000000001111111111111111
+    x = (x | x << 16ul) & 0x1f0000ff0000fful;  // shift left 32 bits, OR with self, and 00011111000000000000000011111111000000000000000011111111
+    x = (x | x << 8ul) & 0x100f00f00f00f00ful; // shift left 32 bits, OR with self, and 0001000000001111000000001111000000001111000000001111000000000000
+    x = (x | x << 4ul) & 0x10c30c30c30c30c3ul; // shift left 32 bits, OR with self, and 0001000011000011000011000011000011000011000011000011000100000000
+    x = (x | x << 2ul) & 0x1249249249249249ul;
     return x;
 }
 
 uint64_t encodeMorton3_64(in uvec3 a) {
-    uint64_t answer = 0;
-    answer |= splitBy3(a.x) | splitBy3(a.y) << 1 | splitBy3(a.z) << 2;
-    return answer;
+    return (splitBy3(a.x) | (splitBy3(a.z) << 1) | (splitBy3(a.y) << 2));
 }
 
 #endif

06841916fce3dff03f6631de048d6a201bfdbc95
EvS <elviras9t@gmail.com>
2018-02-11 20:50:36 +0800

    change tile size with 32-byte rays

--- shaders-sdk/include/rayslib.glsl
+++ shaders-sdk/include/rayslib.glsl
@@ -12,7 +12,7 @@
 #include "../include/ballotlib.glsl"
 
 // paging optimized tiling
-const int R_BLOCK_WIDTH = 32;
+const int R_BLOCK_WIDTH = 64;
 const int R_BLOCK_HEIGHT = 32;
 
 // performance optimized tiling

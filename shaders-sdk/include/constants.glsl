9be089be335989ddbd5b56a91d0304535870eb13
elviras9t <elviras9t@gmail.com>
2018-04-11 02:41:35 +0800

    refactoring of options, nvidia 128 wide

--- shaders-sdk/include/constants.glsl
+++ shaders-sdk/include/constants.glsl
@@ -1,37 +1,37 @@
 #ifndef _CONSTANTS_H
 #define _CONSTANTS_H
 
-    // control flow
-    #extension GL_EXT_control_flow_attributes : enable
 
-    // data format extensions
-    #extension GL_AMD_gcn_shader : enable
-    #extension GL_AMD_gpu_shader_half_float : enable
-    #extension GL_ARB_gpu_shader_int64 : enable
-    #extension GL_AMD_gpu_shader_int16 : enable
+// control flow
+#extension GL_EXT_control_flow_attributes : enable
 
-    // intrinsics extensions
-    #extension GL_AMD_shader_trinary_minmax : enable
-    
-    // texture extensions
-    #extension GL_AMD_texture_gather_bias_lod : enable
-    #extension GL_AMD_shader_image_load_store_lod : enable
-    #extension GL_EXT_shader_image_load_formatted : enable
+// data format extensions
+#extension GL_AMD_gcn_shader : enable
+#extension GL_AMD_gpu_shader_half_float : enable
+#extension GL_ARB_gpu_shader_int64 : enable
+#extension GL_AMD_gpu_shader_int16 : enable
 
-    // subgroup operations
-    #extension GL_KHR_shader_subgroup_basic            : enable
-    #extension GL_KHR_shader_subgroup_vote             : enable
-    #extension GL_KHR_shader_subgroup_arithmetic       : enable
-    #extension GL_KHR_shader_subgroup_ballot           : enable
-    #extension GL_KHR_shader_subgroup_shuffle          : enable
-    #extension GL_KHR_shader_subgroup_shuffle_relative : enable
-    #extension GL_KHR_shader_subgroup_clustered        : enable
+// intrinsics extensions
+#extension GL_AMD_shader_trinary_minmax : enable
+
+// texture extensions
+#extension GL_AMD_texture_gather_bias_lod : enable
+#extension GL_AMD_shader_image_load_store_lod : enable
+#extension GL_EXT_shader_image_load_formatted : enable
+
+// subgroup operations
+#extension GL_KHR_shader_subgroup_basic            : enable
+#extension GL_KHR_shader_subgroup_vote             : enable
+#extension GL_KHR_shader_subgroup_arithmetic       : enable
+#extension GL_KHR_shader_subgroup_ballot           : enable
+#extension GL_KHR_shader_subgroup_shuffle          : enable
+#extension GL_KHR_shader_subgroup_shuffle_relative : enable
+#extension GL_KHR_shader_subgroup_clustered        : enable
 
 
 // ray tracing options
-//#define EXPERIMENTAL_DOF
-#define SUNLIGHT_CAUSTICS false
-#define REFRACTION_SKIP_SUN
+//#define EXPERIMENTAL_DOF // no dynamic control supported
+#define ENABLE_PT_SUNLIGHT
 #define DIRECT_LIGHT_ENABLED
 
 // sampling options
@@ -48,26 +48,20 @@
 #endif
 
 #ifndef ENABLE_AMD_INSTRUCTION_SET
-#undef ENABLE_AMD_INT16 // not supported combination
+    #undef ENABLE_AMD_INT16 // not supported combination
 #endif
 
 #ifndef ENABLE_AMD_INT16
-#undef ENABLE_AMD_INT16_CONDITION // required i16
-#endif
-
-#ifdef ENABLE_NVIDIA_INSTRUCTION_SET
-#extension GL_NV_gpu_shader5 : enable // not supported by SPIRV
+    #undef ENABLE_AMD_INT16_CONDITION // required i16
 #endif
 
 // Platform-oriented compute
 #ifndef WORK_SIZE
-#ifdef ENABLE_AMD_INSTRUCTION_SET
-// 16 working threads, with 1-4 hyper-threaded SIMD in 16 lanes, and depends by hyper-threaded wide, working threads in local group will moved to another work-group (in general, independently works 4-16 work-threads/cores)
-//#define WORK_SIZE 128
-#define WORK_SIZE 64
-#else
-#define WORK_SIZE 64
-#endif
+    #ifdef NVIDIA_PLATFORM
+        #define WORK_SIZE 128
+    #else
+        #define WORK_SIZE 64
+    #endif
 #endif
 
 #define LOCAL_SIZE_LAYOUT layout ( local_size_x = WORK_SIZE ) in
--- shaders-sdk/include/shadinglib.glsl
+++ shaders-sdk/include/shadinglib.glsl
@@ -1,24 +1,19 @@
 #ifndef _SHADINGLIB_H
 #define _SHADINGLIB_H
+
+// gap in hit point
 #define GAP (PZERO*2.f)
-
-
-float computeFresnel(in vec3 normal, in vec3 indc, in float n1, in float n2) {
-    float cosi = dot(normal,  normalize(indc));
-    float cost = dot(normal,  normalize(refract(indc, normal, n1 / n2)));
-    float Rs = fma(n1, cosi, - n2 * cost) / fma(n1, cosi, n2 * cost);
-    float Rp = fma(n1, cost, - n2 * cosi) / fma(n1, cost, n2 * cosi);
-    return sqrt(clamp(sqlen(vec2(Rs, Rp)) * 0.5f, 0.0f, 1.0f));
-}
-
 
 vec3 lightCenter(in int i) {
     return fma(normalize(lightUniform.lightNode[i].lightVector.xyz), lightUniform.lightNode[i].lightVector.www, lightUniform.lightNode[i].lightOffset.xyz);
 }
 
-
-vec3 sLight(in int i) {
+vec3 sphereLightPoint(in int i) {
     return fma(randomDirectionInSphere(), vec3(lightUniform.lightNode[i].lightColor.w - 0.0001f), lightCenter(i));
+}
+
+vec3 getLightColor(in int lc) {
+    return max(lightUniform.lightNode[lc].lightColor.xyz, vec3(0.f));
 }
 
 
@@ -40,11 +35,9 @@ float intersectSphere(in vec3 origin, in vec3 ray, in vec3 sphereCenter, in floa
     return t;
 }
 
-
 float modularize(in float f) {
     return 1.0f-sqrt(max(1.0f - f, 0.f));
 }
-
 
 float samplingWeight(in vec3 ldir, in vec3 ndir, in float radius, in float dist) {
     return modularize(max(dot(ldir, ndir), 0.f) * pow(radius / dist, 2.f)) * 2.f;
@@ -70,7 +63,7 @@ RayRework directLight(in int i, in RayRework directRay, in vec3 color, in mat3 t
     directRay.origin.xyz = fma(ldirect.xyz, vec3(GAP), directRay.origin.xyz);
     WriteColor(directRay.dcolor, f16_f32(directRay.dcolor) * vec4(color,1.f) * vec4(weight.xxx,1.f));
 #else
-    vec3 lpath = sLight(i) - directRay.origin.xyz;
+    vec3 lpath = sphereLightPoint(i) - directRay.origin.xyz;
     vec3 ldirect = normalize(lpath);
     float dist = length(lightCenter(i).xyz - directRay.origin.xyz);
     float weight = samplingWeight(ldirect, tbn[2], lightUniform.lightNode[i].lightColor.w, dist);
@@ -99,14 +92,6 @@ RayRework directLight(in int i, in RayRework directRay, in vec3 color, in mat3 t
 
     return directRay;
 }
-
-
-vec3 normalOrient(in vec3 runit, in mat3 tbn){
-    //vec3 btn = cross(runit, tbn[2]), tng = cross(btn, tbn[2]); btn = cross(tng, tbn[2]);
-    //return normalize(mat3(tng, btn, tbn[2]) * runit);
-    return normalize(tbn * runit);
-}
-
 
 RayRework diffuse(in RayRework ray, in vec3 color, in mat3 tbn) {
     WriteColor(ray.dcolor, f16_f32(ray.dcolor) * vec4(color,1.f));
@@ -158,16 +143,15 @@ RayRework reflection(in RayRework ray, in vec3 color, in mat3 tbn, in float refl
 #ifdef DISABLE_REFLECTIONS
     const int caustics_bounces = 0, reflection_bounces = 0; refly = 0.f;
 #else
-    // bounce mini-config
-#ifdef USE_SIMPLIFIED_MODE
-    const int caustics_bounces = 0, reflection_bounces = 1; refly = 0.f;
-#else
-#ifdef USE_OPTIMIZED_PT
-    const int caustics_bounces = 0, reflection_bounces = 1;
-#else
-    const int caustics_bounces = 0, reflection_bounces = 2;
-#endif
-#endif
+    #ifdef USE_SIMPLIFIED_MODE
+        const int caustics_bounces = 0, reflection_bounces = 1; refly = 0.f;
+    #else
+    #ifdef USE_OPTIMIZED_PT
+        const int caustics_bounces = 0, reflection_bounces = 1;
+    #else
+        const int caustics_bounces = 0, reflection_bounces = 2;
+    #endif
+    #endif
 #endif
 
     if ( RayType(ray) == 1 ) RayDL(ray, true_); // allow to caustics light
@@ -187,41 +171,6 @@ RayRework reflection(in RayRework ray, in vec3 color, in mat3 tbn, in float refl
     IF (not(RayActived(ray))) WriteColor(ray.dcolor, 0.f.xxxx);
 
     return ray;
-}
-
-
-// for future purpose
-RayRework refraction(in RayRework ray, in vec3 color, in mat3 tbn, in float inior, in float outior, in float glossiness) {
-    vec3 refrDir = normalize(  refract( dcts(ray.cdirect.xy) , tbn[2], inior / outior)  );
-    bool_ directDirc = equalF(inior, outior), isShadowed = bool_(RayType(ray) == 2);
-    //if (RayType(ray) == 1 ) RayDL(ray, true_); // allow to caustics light
-
-#ifdef REFRACTION_SKIP_SUN
-    IF (not(isShadowed | directDirc)) {
-        ray.cdirect.xy = lcts(refrDir);
-        RayBounce(ray, max(RayBounce(ray)-(RayType(ray)==3?0:1), 0));
-    }
-#else
-    IF (not(directDirc)) { // if can't be directed
-        ray.cdirect.xy = lcts(refrDir);
-        RayBounce(ray, max(RayBounce(ray)-(RayType(ray)==3?0:1), 0));
-        if ( isShadowed) RayActived(ray, false_); // trying to refract shadows will fails
-        if (!isShadowed) RayType(ray, 0); // can be lighted by direct
-    }
-#endif
-
-    ray.origin.xyz = fma(refrDir, vec3(GAP), ray.origin.xyz);
-    WriteColor(ray.dcolor, f16_f32(ray.dcolor) * vec4(color, 1.f));
-
-    // inactived can't be shaded
-    IF (not(RayActived(ray))) WriteColor(ray.dcolor, 0.f.xxxx);
-
-    return ray;
-}
-
-
-vec3 getLightColor(in int lc) {
-    return max(lightUniform.lightNode[lc].lightColor.xyz, vec3(0.f));
 }
 
 #endif
